1a3fe2668d859538c489a1455fb3d71d0690591a
Paul Evans <decretist@gmail.com>
2014-01-18 08:47:08 -0500

    Deployed as rev-034. Changed variable names (dict to sourceDict, list to sourceList) for compatibility with Python 2.7.

--- src/ingobert.py
+++ src/ingobert.py
@@ -8,7 +8,7 @@ import re
 import string
 import webapp2
 
-dict = {'Aa': 'Admont, Stiftsbibliothek 23 and 43',
+sourceDict = {'Aa': 'Admont, Stiftsbibliothek 23 and 43',
         'Bc': 'Barcelona, Arxiu de la Corona d\'Aragó, Santa Maria de Ripoll 78',
         '5': 'Beinecke 413, 98r-102r',
         '5bis': 'Beinecke 413, 102v-104v',
@@ -60,8 +60,8 @@ class MainPage(webapp2.RequestHandler):
                 Bc = chapter
         template_values = {
             'page_head': 'Gratian, <cite>Decretum</cite>, D. 63, d.p.c. 34',
-            'column_1_head': dict[Aa.source],
-            'column_2_head': dict[Bc.source],
+            'column_1_head': sourceDict[Aa.source],
+            'column_2_head': sourceDict[Bc.source],
             'column_1_body': compare(Aa.text, Bc.text),
             'column_2_body': compare(Bc.text, Aa.text),
         }
@@ -90,8 +90,8 @@ class TwoColumn(webapp2.RequestHandler):
                 column_2_body = compare(column_2.text, '')
         template_values = {
             'page_head': 'Capitulare Carisiacense, cap. ' + self.request.get('chapter'),
-            'column_1_head': dict[self.request.get('column_1')],
-            'column_2_head': dict[self.request.get('column_2')],
+            'column_1_head': sourceDict[self.request.get('column_1')],
+            'column_2_head': sourceDict[self.request.get('column_2')],
             'column_1_body': column_1_body,
             'column_2_body': column_2_body,
         }
@@ -102,16 +102,16 @@ class FourColumn(webapp2.RequestHandler):
         os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
         chapters = ndb.gql('SELECT * FROM Capitulary WHERE chapter = :1', int(self.request.get('chapter')))
         tmps = [None, None, None, None]
-        list = ['5', '5bis', 'Sirmond', 'Boretius']
-        list.remove(self.request.get('comparison'))
+        sourceList = ['5', '5bis', 'Sirmond', 'Boretius']
+        sourceList.remove(self.request.get('comparison'))
         for chapter in chapters:
             if (chapter.source == self.request.get('comparison')):
                 tmps[0] = chapter
-            if (chapter.source == list[0]):
+            if (chapter.source == sourceList[0]):
                 tmps[1] = chapter
-            if (chapter.source == list[1]):
+            if (chapter.source == sourceList[1]):
                 tmps[2] = chapter
-            if (chapter.source == list[2]):
+            if (chapter.source == sourceList[2]):
                 tmps[3] = chapter
         columns = []
         for tmp in tmps:
@@ -119,7 +119,7 @@ class FourColumn(webapp2.RequestHandler):
             if (tmp != None):
                 if (tmp == tmps[0]):
                     column['highlight'] = True
-                column['source'] = dict[tmp.source]
+                column['source'] = sourceDict[tmp.source]
                 if (tmps[0] != None):
                     column['text'] = compare(tmp.text, tmps[0].text)
                 elif (tmps[0] == None):
